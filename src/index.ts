a37bcb8ca5f84db1c6d523007bdc5ddb6f326089
Michiel van Roon <michiel@michielvanroon.nl>
2021-04-10 09:21:42 +0200

    wip improve docs

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,9 @@
 import * as jwt from 'jsonwebtoken'
 import { AxiosRequestConfig } from 'axios'
 
-type RequestsQueue = {
-  resolve: (value?: unknown) => void
-  reject: (reason?: unknown) => void
-}[]
-
-let isRefreshing: boolean = false
-let queue: RequestsQueue = []
-
-const processQueue = (error: Error | null, token: string | null = null) => {
-  queue.forEach((p) => {
-    if (error !== null) {
-      p.reject(error)
-    } else {
-      p.resolve(token)
-    }
-  })
-
-  queue = []
-}
-
 // a little time before expiration to try refresh (seconds)
 const EXPIRE_FUDGE = 10
+const STORAGE_KEY = `auth-tokens-${process.env.NODE_ENV}`
 
 type Token = string
 export interface IAuthTokens {
@@ -36,7 +17,7 @@ export const isLoggedIn = (): boolean => {
   return !!token
 }
 
-export const setAuthTokens = (tokens: IAuthTokens) => localStorage.setItem(getTokenStorageKey(), JSON.stringify(tokens))
+export const setAuthTokens = (tokens: IAuthTokens) => localStorage.setItem(STORAGE_KEY, JSON.stringify(tokens))
 
 export const setAccessToken = (token: Token) => {
   const tokens = getAuthTokens()
@@ -49,12 +30,54 @@ export const setAccessToken = (token: Token) => {
   setAuthTokens(tokens)
 }
 
-export const clearAuthTokens = () => localStorage.removeItem(getTokenStorageKey())
+export const clearAuthTokens = () => localStorage.removeItem(STORAGE_KEY)
+
+export const getRefreshToken = (): Token | undefined => {
+  const tokens = getAuthTokens()
+  return tokens ? tokens.refreshToken : undefined
+}
+export const getAccessToken = (): Token | undefined => {
+  const tokens = getAuthTokens()
+  return tokens ? tokens.accessToken : undefined
+}
+
+/**
+ * @callback requestRefresh
+ * @param {string} refreshToken - Token that is sent to the backend
+ * @returns {Promise} Promise that resolves in an access token
+ */
+
+/**
+ * Get the current access token, exchange it with a new one if it's expired and then returns and return it.
+ * @param {requestRefresh} requestRefresh - Fuction that is used to get a new access token
+ * @returns {string} Access token
+ */
+export const refreshTokenIfNeeded = async (requestRefresh: TokenRefreshRequest): Promise<Token | undefined> => {
+  // use access token (if we have it)
+  let accessToken = getAccessToken()
+
+  // check if access token is expired
+  if (!accessToken || isTokenExpired(accessToken)) {
+    // do refresh
+    accessToken = await refreshToken(requestRefresh)
+  }
+
+  return accessToken
+}
+
+/**
+ *
+ * @param {Axios} axios - Axios instance to apply the interceptor to
+ * @param {IAuthTokenInterceptorConfig} config - Configuration for the interceptor
+ */
+export const useAuthTokenInterceptor = (axios: any, config: IAuthTokenInterceptorConfig) => {
+  if (!axios.interceptors) throw new Error(`invalid axios instance: ${axios}`)
+  axios.interceptors.request.use(authTokenInterceptor(config))
+}
 
 // PRIVATE
-const getTokenStorageKey = (): string => `auth-tokens-${process.env.NODE_ENV}`
 const getAuthTokens = (): IAuthTokens | undefined => {
-  const tokensRaw = localStorage.getItem(getTokenStorageKey())
+  const tokensRaw = localStorage.getItem(STORAGE_KEY)
   if (!tokensRaw) return
 
   try {
@@ -65,30 +88,21 @@ const getAuthTokens = (): IAuthTokens | undefined => {
   }
   return
 }
-
-export const getRefreshToken = (): Token | undefined => {
-  const tokens = getAuthTokens()
-  return tokens ? tokens.refreshToken : undefined
-}
-export const getAccessToken = (): Token | undefined => {
-  const tokens = getAuthTokens()
-  return tokens ? tokens.accessToken : undefined
-}
 const isTokenExpired = (token: Token): boolean => {
   if (!token) return true
-  const expin = getExpiresInFromJWT(token) - EXPIRE_FUDGE
-  return !expin || expin < 0
+  const expiration = getExpiration(token) - EXPIRE_FUDGE
+  return !expiration || expiration < 0
 }
 
 // gets unix TS
-const getTokenExpiresTimeStamp = (token: Token): number | undefined => {
+const getTimestampFromToken = (token: Token): number | undefined => {
   const decoded = jwt.decode(token)
   if (!decoded) return
   return (decoded as { [key: string]: number }).exp
 }
 
-const getExpiresInFromJWT = (token: Token): number => {
-  const exp = getTokenExpiresTimeStamp(token)
+const getExpiration = (token: Token): number => {
+  const exp = getTimestampFromToken(token)
   if (exp) return exp - Date.now() / 1000
 
   return -1
@@ -111,7 +125,7 @@ const refreshToken = async (requestRefresh: TokenRefreshRequest): Promise<Token>
     // failed to refresh... check error type
     if (err && err.response && (err.response.status === 401 || err.response.status === 422)) {
       // got invalid token response for sure, remove saved tokens because they're invalid
-      localStorage.removeItem(getTokenStorageKey())
+      localStorage.removeItem(STORAGE_KEY)
       return Promise.reject(`Got 401 on token refresh; Resetting auth token: ${err}`)
     } else {
       // some other error, probably network error
@@ -123,12 +137,42 @@ const refreshToken = async (requestRefresh: TokenRefreshRequest): Promise<Token>
 }
 
 export type TokenRefreshRequest = (refreshToken: string) => Promise<Token>
+
+type RequestsQueue = {
+  resolve: (value?: unknown) => void
+  reject: (reason?: unknown) => void
+}[]
+
+let isRefreshing: boolean = false
+let queue: RequestsQueue = []
+
+const processQueue = (error: Error | null, token: string | null = null) => {
+  queue.forEach((p) => {
+    if (error !== null) {
+      p.reject(error)
+    } else {
+      p.resolve(token)
+    }
+  })
+
+  queue = []
+}
+
 export interface IAuthTokenInterceptorConfig {
   header?: string
   headerPrefix?: string
   requestRefresh: TokenRefreshRequest
 }
 
+/**
+ * Function that returns an Axios Intercepter that:
+ * - Applies that right auth header to requests
+ * - Refreshes the access token when needed
+ * - Puts subsequent requests in a queue and executes them after the access token has been refreshed.
+ *
+ * @param {IAuthTokenInterceptorConfig} config - Configuration for the interceptor
+ * @returns {Promise} Promise that resolves in the supplied requestConfig
+ */
 const authTokenInterceptor = ({
   header = 'Authorization',
   headerPrefix = 'Bearer ',
@@ -166,22 +210,4 @@ const authTokenInterceptor = ({
   // add token to headers
   if (accessToken) requestConfig.headers[header] = `${headerPrefix}${accessToken}`
   return requestConfig
-}
-
-export const refreshTokenIfNeeded = async (requestRefresh: TokenRefreshRequest): Promise<Token | undefined> => {
-  // use access token (if we have it)
-  let accessToken = getAccessToken()
-
-  // check if access token is expired
-  if (!accessToken || isTokenExpired(accessToken)) {
-    // do refresh
-    accessToken = await refreshToken(requestRefresh)
-  }
-
-  return accessToken
-}
-
-export const useAuthTokenInterceptor = (axios: any, config: IAuthTokenInterceptorConfig) => {
-  if (!axios.interceptors) throw new Error(`invalid axios instance: ${axios}`)
-  axios.interceptors.request.use(authTokenInterceptor(config))
 }
